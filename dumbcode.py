0a255dced3f72d60c710df6bb3b631d2b19aae5f
Kate <Katezador-ipt27@lll.kpi.ua>
2024-12-16 23:59:05 +0200

    add github actions pipeline

--- dumbcode.py
+++ dumbcode.py
@@ -3,3 +3,8 @@ def square (x):
 
 def cube (x):
     return x * x * x
+
+if __name__ == "__main__":
+    a = 3
+    print("Квадрат числа", a, ":", square (a))
+    print("Куб числа", a, ":", cube (a))
